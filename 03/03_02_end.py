d771aa5118cf723d9b6c0ea9574cc177c57c9df8
SinghPragya24 <80520824+SinghPragya24@users.noreply.github.com>
2024-05-12 15:52:45 +0530

    Update 03_02_end.py

--- 03/03_02_end.py
+++ 03/03_02_end.py
@@ -1,20 +1,20 @@
-import os
-import numpy
+# Python code​​​​​​‌​‌‌​​​‌‌​‌​​‌​‌​‌‌​‌​​​‌ below
+show_expected_result = False
 import pickle
-from numpy import dot
-from numpy.linalg import norm
-
-
-CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
-
-with open(CURRENT_DIR + "/word_to_vector_trsf.pkl", "rb") as pk:
+with open("./deps/deps/word_to_vector_trsf.pkl", "rb") as pk:
     word_to_vector = pickle.load(pk)
 
 def cosine_similarity(vec_a, vec_b):
     numerator = sum([vec_a[i] * vec_b[i] for i in range(len(vec_a))])
-    denominator = (norm(vec_a) * norm(vec_b))
+    denominator = (sum([vec_a[i] ** 2 for i in range(len(vec_a))]) ** 0.5 * sum([vec_b[i] ** 2 for i in range(len(vec_b))]) ** 0.5)
     return numerator / denominator
 
-print(cosine_similarity(word_to_vector["plant"], word_to_vector["grow"]))
-print(cosine_similarity(word_to_vector["minute"], word_to_vector["plant"]))
-print(cosine_similarity(word_to_vector["plant"], word_to_vector["tree"]))
+show_expected_result = False
+show_hints = False
+
+
+def similar_words(word="tree", top_k=10):
+    return sorted(
+        word_to_vector.keys(), 
+        key=lambda x: -cosine_similarity(word_to_vector[x], word_to_vector[word])
+    )[:top_k]
